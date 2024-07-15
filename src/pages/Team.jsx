b389a717ab12258544e9a18fa869984672f51cc2
readwanmd <readwanmd@gmail.com>
2024-07-15 14:42:37 +0600

    Team page created

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,4 +1,30 @@
-const Team = () => {
-	return <div>Team</div>;
-};
+import { IconPhone } from '@tabler/icons-react';
+import { HeroPages } from '../components';
+import TeamContainer from '../components/team/TeamContainer';
+
+function Team() {
+	return (
+		<>
+			<section className="team-page">
+				<HeroPages name="Our Team" />
+				<div className="container">
+					<TeamContainer />
+				</div>
+				<div className="book-banner">
+					<div className="book-banner__overlay"></div>
+					<div className="container">
+						<div className="text-content">
+							<h2>Book a car by getting in touch with us</h2>
+							<span>
+								<IconPhone width={40} height={40} />
+								<h3>(123) 456-7869</h3>
+							</span>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
+}
+
 export default Team;
