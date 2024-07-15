8ec2b4f231a3832a65e97451dc9f67388caacdf7
readwanmd <readwanmd@gmail.com>
2024-07-15 13:57:38 +0600

    testimonials page created

--- src/pages/TestimonialsPage.jsx
+++ src/pages/TestimonialsPage.jsx
@@ -1,4 +1,26 @@
+import { IconPhone } from '@tabler/icons-react';
+import { HeroPages, Testimonials } from '../components';
+
 const TestimonialsPage = () => {
-	return <div>TestimonialsPage</div>;
+	return (
+		<>
+			<section className="testimonial-page">
+				<HeroPages name="Testimonials" />
+				<Testimonials />
+				<div className="book-banner">
+					<div className="book-banner__overlay"></div>
+					<div className="container">
+						<div className="text-content">
+							<h2>Book a car by getting in touch with us</h2>
+							<span>
+								<IconPhone width={40} height={40} />
+								<h3>(123) 456-7869</h3>
+							</span>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 };
 export default TestimonialsPage;
