bb2b09c7362c1c6ffe28fd087aab9cac641325ef
readwanmd <readwanmd@gmail.com>
2024-07-31 08:08:37 +0600

    banner component translated

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,12 +1,18 @@
+import { Trans, useTranslation } from 'react-i18next';
+
 const Banner = () => {
+	const { t } = useTranslation('banner');
+	const title = t('title');
+	const subtitle = t('subtitle');
+
 	return (
 		<section className="banner-section">
 			<div className="container">
 				<div className="banner-content">
 					<div className="banner-content__text">
-						<h2>Save big with our cheap car rental!</h2>
+						<h2>{title}</h2>
 						<p>
-							Top Airports. Local Suppliers. <span>24/7</span> Support.
+							<Trans i18nKey={subtitle} components={{ 1: <span /> }} />
 						</p>
 					</div>
 				</div>
