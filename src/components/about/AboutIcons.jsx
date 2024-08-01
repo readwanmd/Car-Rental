0ad094f68a7dac4ddf9fb26857222987a7860786
readwanmd <readwanmd@gmail.com>
2024-08-01 07:15:27 +0600

    about component translated

--- src/components/about/AboutIcons.jsx
+++ src/components/about/AboutIcons.jsx
@@ -1,27 +1,31 @@
+import { useTranslation } from 'react-i18next';
 import { icon1, icon2, icon3 } from '/public';
 
 function AboutIcons() {
+	const { t } = useTranslation('about');
+	const { card1, card2, card3 } = t('cards');
+
 	return (
 		<div className="about-main__text__icons">
 			<div className="about-main__text__icons__box">
 				<img src={icon1} alt="car-icon" />
 				<span>
-					<h4>20</h4>
-					<p>Car Types</p>
+					<h4>{card1.count}</h4>
+					<p>{card1.title}</p>
 				</span>
 			</div>
 			<div className="about-main__text__icons__box">
 				<img src={icon2} alt="car-icon" />
 				<span>
-					<h4>85</h4>
-					<p>Rental Outlets</p>
+					<h4>{card2.count}</h4>
+					<p>{card2.title}</p>
 				</span>
 			</div>
 			<div className="about-main__text__icons__box">
 				<img src={icon3} alt="car-icon" className="last-fk" />
 				<span>
-					<h4>75</h4>
-					<p>Repair Shop</p>
+					<h4>{card3.count}</h4>
+					<p>{card3.title}</p>
 				</span>
 			</div>
 		</div>
--- src/components/about/AboutMain.jsx
+++ src/components/about/AboutMain.jsx
@@ -1,19 +1,19 @@
+import { useTranslation } from 'react-i18next';
 import AboutIcons from './AboutIcons';
 import { aboutMain } from '/public';
 
 function AboutMain() {
+	const { t } = useTranslation('about');
+	const line1 = t('line1');
+	const title = t('title');
+	const desc = t('desc');
 	return (
 		<div className="about-main">
 			<img className="about-main__img" src={aboutMain} alt="car-renting" />
 			<div className="about-main__text">
-				<h3>About Company</h3>
-				<h2>You start the engine and your adventure begins</h2>
-				<p>
-					Certain but she but shyness why cottage. Guy the put instrument sir
-					entreaties affronting. Pretended exquisite see cordially the you.
-					Weeks quiet do vexed or whose. Motionless if no to affronting
-					imprudence no precaution. My indulged as disposal strongly attended.
-				</p>
+				<h3>{line1}</h3>
+				<h2>{title}</h2>
+				<p>{desc}</p>
 				<AboutIcons />
 			</div>
 		</div>
