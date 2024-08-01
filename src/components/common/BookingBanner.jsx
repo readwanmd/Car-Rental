f0ddc617446a973b8b73153ad68c03958d51665e
readwanmd <readwanmd@gmail.com>
2024-08-01 07:20:52 +0600

    BookingBanner component translated

--- src/components/common/BookingBanner.jsx
+++ src/components/common/BookingBanner.jsx
@@ -1,12 +1,16 @@
 import { IconPhone } from '@tabler/icons-react';
+import { useTranslation } from 'react-i18next';
 
 const BookingBanner = () => {
+	const { t } = useTranslation('bookingBanner');
+	const title = t('title');
+
 	return (
 		<div className="book-banner">
 			<div className="book-banner__overlay"></div>
 			<div className="container">
 				<div className="text-content">
-					<h2>Book a car by getting in touch with us</h2>
+					<h2>{title}</h2>
 					<span>
 						<IconPhone width={40} height={40} />
 						<h3>(123) 456-7869</h3>
