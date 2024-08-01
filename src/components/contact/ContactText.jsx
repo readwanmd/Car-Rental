11e7fdb748661920dd2fbd992b68df86cb4e19c4
readwanmd <readwanmd@gmail.com>
2024-08-01 09:22:38 +0600

    contact design fixed

--- src/components/contact/ContactText.jsx
+++ src/components/contact/ContactText.jsx
@@ -1,6 +1,5 @@
 import { IconLocation, IconMail, IconPhone } from '@tabler/icons-react';
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
 
 function ContactText() {
 	const { t } = useTranslation('contact_us');
@@ -11,16 +10,18 @@ function ContactText() {
 		<div className="contact-div__text">
 			<h2>{title}</h2>
 			<p>{desc}</p>
-			<Link>
-				<IconPhone /> &nbsp; (123) 456-7869
-			</Link>
-			<Link>
-				<IconMail /> &nbsp; carrental@carmail.com
-			</Link>
-			<Link>
-				<IconLocation />
-				&nbsp; Belgrade, Serbia
-			</Link>
+			<div className="links">
+				<a>
+					<IconPhone /> &nbsp; (123) 456-7869
+				</a>
+				<a>
+					<IconMail /> &nbsp; carrental@carmail.com
+				</a>
+				<a>
+					<IconLocation />
+					&nbsp; Belgrade, Serbia
+				</a>
+			</div>
 		</div>
 	);
 }
